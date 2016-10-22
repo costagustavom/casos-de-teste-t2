8e45f59c55d9c84800e32dd9da24e1c2a40b31e5
genicleito <genicleito@live.com>
2016-10-22 13:00:14 -0300

    Adicionado input-10

--- scriptGenicleito.sh
+++ scriptGenicleito.sh
@@ -1,9 +1,9 @@
-for i in `seq 1 9`; do
+for i in `seq 1 10`; do
         echo -e "Rodando Teste: $i"
         ../a.out < entradas/input-$i > saidas/genicleito/output-$i
 done
 
-for i in `seq 1 9`; do
+for i in `seq 1 10`; do
     echo -e "Comparing: $i"
     diff saidas/genicleito/output-$i saidas/jonathan/output-$i
 done
